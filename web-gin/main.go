bd6ec1ebd51c9e22a39b5ff0277bacb90f9f05e5
hujinfeng <pbfoxhu@gmail.com>
2022-03-25 17:14:12 +0800

    gin web demo

---
+++ web-gin/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	router := router()
+	router.Run()
+}
---
+++ web-gin/user.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+)
+
+type User struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type Message struct {
+	User
+	Msg string `json:"message"`
+}
+
+func (u *User) say(msg string) *Message {
+	ret := Message{
+		*u, u.Name + " said, " + msg,
+	}
+	return &ret
+}
+
+func UserSay(c *gin.Context) {
+	action := c.Query("action")
+	if action != "say" {
+		c.JSON(http.StatusExpectationFailed, "Not Support")
+		return
+	}
+
+	name := c.Query("name")
+	user := User{
+		rand.Int63(), name,
+	}
+	msg := c.Query("msg")
+	message := user.say(msg)
+
+	c.JSON(http.StatusOK, message)
+}
