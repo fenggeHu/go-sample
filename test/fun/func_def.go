de0f28e64e1ce7a774fee2115a805531e12a0a51
hujinfeng <pbfoxhu@gmail.com>
2022-04-03 20:46:43 +0800

    func参数

---
+++ test/fun/func_def.go
@@ -0,0 +1,17 @@
+package fun
+
+import "errors"
+
+type Who struct {
+	Name string
+}
+
+// Go 函数的返回值或结果 “形参” 可被命名，并作为常规变量使用，就像传入的形参一样。
+//命名后，一旦该函数开始执行，它们就会被初始化为与其类型相应的零值； 若该函数执行了一条不带实参的 return 语句，则结果形参的当前值将被返回。
+func (w *Who) Say(word string) (message string, err error) {
+	if w == nil || w.Name == "" {
+		err = errors.New("nobody is here")
+	}
+	message = w.Name + " say: " + word
+	return
+}
--- test/main.go
+++ test/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"go-sample/test/fun"
 )
 
+// 在goland run时，不会主动编译main包下的其它文件， 所以把其它.go文件跟main分开
+// 但是通过go命令主动编译可以正常的，如go build *.go
 func main() {
+	who := fun.Who{} //  who := new(Who)
+	s, err := who.Say("hello")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(s)
+	}
 
 	//// if
 	//if f, err := os.OpenFile("/Users/hujinfeng/test/pom.xml", os.O_RDONLY, 0666); err != nil {
@@ -51,19 +61,19 @@ func main() {
 	//}
 
 	// switch
-	mm := map[int]interface{}{1: "1111", 2: 2222, 3: 3.333, 4: false}
-	for _, v := range mm {
-		switch t := v.(type) {
-		default:
-			fmt.Printf("unexpected type %T\n", t) // %T prints whatever type t has
-		case bool:
-			fmt.Printf("boolean %t\n", t) // t has type bool
-		case int:
-			fmt.Printf("integer %d\n", t) // t has type int
-		case *bool:
-			fmt.Printf("pointer to boolean %t\n", *t) // t has type *bool
-		case *int:
-			fmt.Printf("pointer to integer %d\n", *t) // t has type *int
-		}
-	}
+	//mm := map[int]interface{}{1: "1111", 2: 2222, 3: 3.333, 4: false}
+	//for _, v := range mm {
+	//	switch t := v.(type) {
+	//	default:
+	//		fmt.Printf("unexpected type %T\n", t) // %T prints whatever type t has
+	//	case bool:
+	//		fmt.Printf("boolean %t\n", t) // t has type bool
+	//	case int:
+	//		fmt.Printf("integer %d\n", t) // t has type int
+	//	case *bool:
+	//		fmt.Printf("pointer to boolean %t\n", *t) // t has type *bool
+	//	case *int:
+	//		fmt.Printf("pointer to integer %d\n", *t) // t has type *int
+	//	}
+	//}
 }
