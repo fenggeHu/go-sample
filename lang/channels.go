9dd01b5838edfee41570d79caac10e6a3f313e71
hujinfeng <pbfoxhu@gmail.com>
2022-04-20 07:45:26 +0800

    select channels共同消费

--- lang/channels.go
+++ lang/channels.go
@@ -8,7 +8,7 @@ import (
 // 下面receiver和selectChan都接收了c，但1条信息只消费1次
 func channelMain() {
 	c := make(chan string, 100)
-	go ponger(c)
+	//go ponger(c)
 	go receiver(c)
 	go sender("tag-c", c)
 	c2 := make(chan string, 20)
@@ -45,10 +45,16 @@ func selectChan(c1 chan string, c2 chan string, c3 chan string) {
 		select {
 		case m1 := <-c1:
 			fmt.Printf("select c1: %s\n", m1)
+		case <-c1: // 共同消费c1，也是有效的
+			fmt.Printf("select c1: %s\n", time.Now())
 		case m2 := <-c2:
 			fmt.Printf("select c2: %s\n", m2)
 		case m3 := <-c3:
 			fmt.Printf("select c3: %s\n", m3)
+		case m := <-time.After(time.Second):
+			fmt.Printf("timeout: %s\n", m)
+			//default:		// The default case happens immediately if none of the channels are ready。没有业务逻辑的时候不要
+			//	fmt.Println("default")
 		}
 	}
 }
