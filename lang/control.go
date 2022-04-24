d84871a8e7572733ba048d1e5f1d3ec98d35ec23
hujinfeng <pbfoxhu@gmail.com>
2022-04-24 13:41:26 +0800

    类型转换

--- lang/control.go
+++ lang/control.go
@@ -12,9 +12,17 @@ func controlMain() {
 }
 
 func forIfMe() {
-	for i := 0; i < 100; i++ {
+	var x = []int{1, 3, 5, 7, 13}
+	var total float64 = 0
+	for i, value := range x {
+		fmt.Printf("x第%d个element是%d\n", i+1, value)
+		total += float64(value)
+	}
+	fmt.Printf("把x所有元素平均得到%f\n", total/float64(len(x)))
+
+	for i := 1; i < 100; i++ {
 		if i%10 == 0 {
-			fmt.Printf("%d, its multiple of 10", i)
+			fmt.Printf("%d, its multiple of 10\n", i)
 		}
 	}
 }
