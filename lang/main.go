78917b145ad4056abd4fc5ba6055d1c97d4e040b
hujinfeng <pbfoxhu@gmail.com>
2022-04-24 16:14:15 +0800

    flag用法

--- lang/main.go
+++ lang/main.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
+	parseArgs()
+
 	//index := 100
 	//start := time.Now().UnixMilli()
 	//num := FibonacciSequence(int64(index))
@@ -18,4 +25,21 @@ func main() {
 
 	//var input string
 	//fmt.Scanln(&input)
+}
+
+// program arguments format：
+// 单横杆或双横杆+参数名 + 等号或空格 + 值，格式可以混用
+// -name=hu	/ -name hu
+// --name=hu / --name hu
+func parseArgs() {
+	// 2种赋值方式
+	var count int
+	flag.IntVar(&count, "count", 10, "count")
+	name := flag.String("name", "", "Input your name")
+	flag.Parse() // 【必须调用】从 arguments 中解析注册的 flag
+
+	fmt.Printf("my name is %s\n", *name)
+	for i := 0; i < count; i++ {
+		fmt.Println(i)
+	}
 }
