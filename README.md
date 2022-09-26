a14a839a19802519e5e72e70d5ddcbc3d756b64b
hujinfeng <pbfoxhu@gmail.com>
2022-09-26 08:46:16 +0800

    extend_test

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# example
+- 继承：extend_test
+- 抽象类：在struct中加入func成员，参考Man(extend_test)
+
+
 # go-sample
 打包命令, 编译websocket目录下的所有文件，生产可执行文件ws
 ```shell
--- test/extend_test.go
+++ test/extend_test.go
@@ -9,6 +9,8 @@ func TestExtends(t *testing.T) {
 	// error
 	//p := &Parent
 	//m := &Me{Parent{Man{"me"}}}
+	m := &GrandSon{}
+	m.Say("Win")
 }
 
 func (m *Man) Relation(m2 *Man) {
@@ -17,20 +19,56 @@ func (m *Man) Relation(m2 *Man) {
 
 type Man struct {
 	Name string
+	// 定义一个空方法，子类实现这个方法。 达到类似Java的抽象类的效果
+	Greeting func()
+}
+type IMan interface {
+	Say(message string)
+}
+
+func (m *Man) Say(message string) {
+	m.Greeting = func() {
+		fmt.Println("Man said, Hello")
+	}
+	m.Greeting()
+	fmt.Printf("Man: %s\n", message)
 }
 
 type Parent struct {
 	Man
 }
 
+// 实现了Man的抽象方法Greeting
+func (m *Parent) Greeting() {
+	fmt.Printf("Hello\n")
+}
+
+func (m *Parent) Say(message string) {
+	fmt.Printf("Parent: %s\n", message)
+}
+
 type Me struct {
 	Parent
+}
+
+func (m *Me) Say(message string) {
+	fmt.Printf("Me: %s\n", message)
 }
 
 type Son struct {
 	Me
 }
 
+//func (m *Son) Say(message string) {
+//	fmt.Printf("Son: %s\n", message)
+//}
+
 type GrandSon struct {
 	Son
+}
+
+func (m *GrandSon) Say(message string) {
+	m.Son.Say(message)
+	m.Greeting()
+	fmt.Printf("GrandSon: %s\n", message)
 }
