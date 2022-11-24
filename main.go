87f7926069a36f56041ee247bec7b44f7c1da286
hujinfeng <pbfoxhu@gmail.com>
2022-11-24 16:18:34 +0800

    add init function

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
-import "go-sample/test"
+import (
+	"flag"
+	"fmt"
+	"go-sample/test"
+)
+
+var (
+	vers    *bool
+	help    *bool
+	conf    *string
+	testing *string
+)
+
+// function init run before main
+func init() {
+	vers = flag.Bool("v", false, "display the version.")
+	help = flag.Bool("h", false, "print this help.")
+	conf = flag.String("f", "", "specify configuration file.")
+	testing = flag.String("t", "", "test configuration.")
+	flag.Parse()
+
+	fmt.Println(*vers, *help, *conf, *testing)
+}
 
 func main() {
 	test.Pi()
